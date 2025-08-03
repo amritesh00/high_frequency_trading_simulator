1579e7e06bb9bbe4c6f354a059bf203dff5d3503
amritesh bhukhmaria <amritfacitindia@gmail.com>
2025-08-03 22:38:53 +0530

    Update orderbook.py

--- app/orderbook.py
+++ app/orderbook.py
@@ -1,21 +1,17 @@
 # app/orderbook.py
+
 import json
-from . import rdb
 
-ORDERBOOK_KEY = "orderbook"
+class Order:
+    def __init__(self, type, price, quantity):
+        self.type = type
+        self.price = price
+        self.quantity = quantity
 
-def add_order(order_type, price, quantity):
-    order = {"type": order_type, "price": float(price), "quantity": int(quantity)}
-    rdb.rpush(ORDERBOOK_KEY, json.dumps(order))
+    def to_json(self):
+        return json.dumps(self.__dict__)
 
-def get_orderbook():
-    data = rdb.lrange(ORDERBOOK_KEY, 0, -1)
-    buy_orders = []
-    sell_orders = []
-    for entry in data:
-        order = json.loads(entry)
-        if order['type'] == 'buy':
-            buy_orders.append(order)
-        else:
-            sell_orders.append(order)
-    return {"buy": buy_orders, "sell": sell_orders}
+    @staticmethod
+    def from_json(json_str):
+        data = json.loads(json_str)
+        return Order(data["type"], data["price"], data["quantity"])
