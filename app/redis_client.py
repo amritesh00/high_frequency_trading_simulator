1da6f47467a23ef1f7ae29799f68d5721fce821e
amritesh bhukhmaria <amritfacitindia@gmail.com>
2025-08-03 22:35:11 +0530

    Create redis_client.py

---
+++ app/redis_client.py
@@ -0,0 +1,14 @@
+# app/redis_client.py
+
+import redis
+import os
+from urllib.parse import urlparse
+
+redis_url = urlparse(os.getenv("REDIS_URL", "redis://localhost:6379"))
+
+rdb = redis.Redis(
+    host=redis_url.hostname,
+    port=redis_url.port,
+    password=redis_url.password,
+    decode_responses=True
+)
