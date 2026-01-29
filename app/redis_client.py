adc343891f038d5b8022f4b41553c8b5d462dc3b
amritesh bhukhmaria <amritfacitindia@gmail.com>
2026-01-29 12:02:53 +0530

    Update redis_client.py

--- app/redis_client.py
+++ app/redis_client.py
@@ -7,7 +7,7 @@ redis_url = urlparse(os.getenv("REDIS_URL"))
 rdb = redis.Redis(
     host=redis_url.hostname,
     port=redis_url.port,
-    username=redis_url.username,   # IMPORTANT
-    password=redis_url.password,
+    username=redis_url.username,
+    password=redis_url.password,   
     decode_responses=True
 )
