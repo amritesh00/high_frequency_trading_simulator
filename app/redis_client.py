eddd6a7b1e9584e325c112b93515f6b5444eb897
amritesh bhukhmaria <amritfacitindia@gmail.com>
2026-01-30 12:47:34 +0530

    Update redis_client.py

--- app/redis_client.py
+++ app/redis_client.py
@@ -1,25 +1,16 @@
 import os
 import redis
 
-REDIS_HOST = "redis-17677.c326.us-east-1-3.ec2.cloud.redislabs.com"
-REDIS_PORT = 17677
-REDIS_USERNAME = "default"
-REDIS_PASSWORD = "Heatma@009"
+REDIS_URL = os.getenv("REDIS_URL")
 
-rdb = redis.Redis(
-    host=REDIS_HOST,
-    port=REDIS_PORT,
-    username=REDIS_USERNAME,
-    password=REDIS_PASSWORD,
-    ssl=True,
-    ssl_cert_reqs=None,
+rdb = redis.Redis.from_url(
+    REDIS_URL,
     decode_responses=True,
 )
 
-# Optional: test lazily (do NOT crash app)
-def redis_ping():
+def redis_ok():
     try:
         return rdb.ping()
     except Exception as e:
-        print("Redis connection failed:", e)
+        print("Redis error:", e)
         return False
